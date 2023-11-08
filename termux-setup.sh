41fcf3a1d991a67e430373cf1e10979e3b348e2e
clecio81 <55952484+clecio81@users.noreply.github.com>
2023-11-08 14:44:23 -0300

    Update termux-setup.sh

--- termux-setup.sh
+++ termux-setup.sh
@@ -14,10 +14,10 @@ fi
 # URL / BRANCH might be overwritten here
 . config.env
 
-wget -q -c -t0 ${URL}/${BRANCH}/answerfile
-wget -q -c -t0 ${URL}/${BRANCH}/ssh2qemu.sh
+wget -q -c -t0 https://raw.githubusercontent.com/clecio81/qemu-alpine-docker_on_termux/main/answerfile
+wget -q -c -t0 https://raw.githubusercontent.com/clecio81/qemu-alpine-docker_on_termux/main/ssh2qemu.sh
 chmod +x ./ssh2qemu.sh
-wget -q -c -t0 ${URL}/${BRANCH}/startqemu.sh
+wget -q -c -t0 https://raw.githubusercontent.com/clecio81/qemu-alpine-docker_on_termux/main/startqemu.sh
 chmod +x ./startqemu.sh
-wget -q -c -t0 ${URL}/${BRANCH}/installqemu.expect
+wget -q -c -t0 https://raw.githubusercontent.com/clecio81/qemu-alpine-docker_on_termux/main/installqemu.expect
 expect -f installqemu.expect
